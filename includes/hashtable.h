f80ecb369cde983e53e492991c84a90edbcdf320
Giacomo Trapani <giacomo.trapani01@libero.it>
2021-06-12 21:41:52 +0200

    Aggiunta la struttura dati tabella hash.

---
+++ includes/hashtable.h
@@ -0,0 +1,97 @@
+/**
+ * @brief Header file for hash table data structure.
+ * @author Giacomo Trapani.
+*/
+
+#ifndef _HASHTABLE_H_
+#define _HASHTABLE_H_
+
+#include <stdlib.h>
+#include <linked_list.h>
+
+/**
+ * @brief Initializes empty hash table struct given number of buckets.
+ * @returns Pointer to initialized table on success, NULL on failure.
+ * @exception It sets errno to ENOMEM if and only if needed memory allocation fails.
+*/
+#define HASHTABLE_INITIALIZER(buckets_no) HashTable_Init(buckets_no, NULL, NULL);
+
+// Struct fields are not exposed to maintain invariant.
+typedef struct _hashtable hashtable_t;
+
+/**
+ * @brief Polynomial rolling hash function.
+ * @param buffer buffer to hash
+ * @return hash value of input buffer
+*/
+size_t
+HashTable_HashFunction(const void*);
+
+/**
+ * String comparison.
+*/
+int
+HashTable_Compare(const void*, const void*);
+
+/**
+ * @brief Initializes empty hash table struct given number of buckets, 
+ * hash function and compare function.
+ * @param buckets_no number of buckets
+ * @param hash_function pointer to the function used for hashing; if NULL is passed then
+ * it uses a default provided polynomial rolling hash function.
+ * @param hash_compare pointer to the function used for key comparison; if NULL is passed then
+ * it uses a default provided string comparison function.
+ * @returns Pointer to initialized table on success, NULL on failure.
+ * @exception It sets errno to ENOMEM if and only if needed memory allocation fails.
+*/
+hashtable_t*
+HashTable_Init(size_t, size_t (const void*), int (const void*, const void*));
+
+/**
+ * @brief Creates and inserts entry into hashtable. Duplicates are not allowed.
+ * @returns 1 on successful insertion, 0 if it is a duplicate, -1 otherwise. 
+ * @exception It sets errno to ENOMEM if and only if needed memory allocation fails,
+ * (it sets errno to) EINVAL if and only if key is NULL or key_size is zero.
+*/
+int
+HashTable_Insert(hashtable_t*, const void*, size_t, const void*, size_t);
+
+/**
+ * @brief Checks whether hash table contains an entry for given key.
+ * @returns 1 if such entry exists, 0 if it does not, -1 on failure.
+ * @exception It sets errno to EINVAL if table or entry are NULL.
+*/
+int
+HashTable_Find(const hashtable_t*, const void*);
+
+/**
+ * @brief Gets data corresponding to the given key, it will be written inside third param
+ * (which must not refer to a pre-allocated buffer). Returns data size.
+ * @returns Size of data buffer on success, 0 on failure.
+ * @exception It sets errno to EINVAL if and only if table or entry is NULL,
+ * (it sets errno to) ENOMEM if and only if needed memory allocation fails.
+*/
+size_t
+HashTable_GetData(const hashtable_t*, const void*, void**);
+
+/**
+ * @brief Deletes node corresponding to given key from hash table.
+ * @returns 1 on successful deletion, 0 if there was not such an entry, -1 on failure.
+ * @exception It sets errno to EINVAL if and only if table or entry are NULL.
+*/
+int
+HashTable_DeleteNode(hashtable_t*, const void*);
+
+/**
+ * Frees allocated resources.
+*/
+void
+HashTable_Free(hashtable_t*);
+
+/**
+ * Utility print function.
+*/
+void
+HashTable_Print(const hashtable_t*);
+
+#endif
---
+++ src/data_structures/hashtable.c
@@ -0,0 +1,232 @@
+/**
+ * @brief Source file for hashtable header.
+ * @author Giacomo Trapani.
+*/
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <node.h>
+#include <hashtable.h>
+#include <linked_list.h>
+
+struct _hashtable
+{
+	size_t buckets_no;
+	linked_list_t** buckets; 
+	size_t (*hash_function) (const void*);
+	int (*hash_compare) (const void*, const void*);
+};
+
+size_t 
+HashTable_HashFunction(const void* buffer)
+{
+	if (buffer == NULL) return 0;
+	char* tmp = (char*) buffer;
+	// roughly equal to the number of letters
+	// in the alphabet (uppercase and lowercase)
+	const int p = 53;
+	const int m = 1e9 + 9; // large prime number
+	size_t hash_value = 0;
+	long long p_pow = 1;
+	for (size_t i = 0; tmp[i] != '\0'; i++)
+	{
+		char c = tmp[i];
+		hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+		p_pow = (p_pow * p) % m;
+	}
+	return hash_value;
+}
+
+int
+HashTable_Compare(const void* a, const void* b)
+{
+	char* buffer1 = (char*) a;
+	char* buffer2 = (char*) b;
+	return strcmp(buffer1, buffer2);
+}
+
+hashtable_t*
+HashTable_Init(size_t buckets_no, size_t (*hash_function) (const void*), 
+		int (*hash_compare) (const void*, const void*))
+{
+	hashtable_t* table;
+	table = (hashtable_t*) malloc(sizeof(hashtable_t));
+	if (table == NULL)
+	{
+		errno = ENOMEM;
+		return NULL;
+	}
+	table->buckets_no = buckets_no;
+	table->buckets = (linked_list_t**) malloc(sizeof(linked_list_t*) * buckets_no);
+	if (!(table->buckets))
+	{
+		errno = ENOMEM;
+		free(table);
+		return NULL;
+	}
+	size_t i = 0;
+	for (; i < buckets_no; i++)
+	{
+		(table->buckets)[i] = LinkedList_Init();
+		if (!((table->buckets)[i]))
+		{
+			size_t j = 0;
+			while (j != i)
+			{
+				LinkedList_Free((table->buckets)[j]);
+				j++;
+			}
+			free(table->buckets);
+			free(table);
+			errno = ENOMEM;
+			return NULL;
+		}
+	}
+	table->hash_function = ((!hash_function) ? (HashTable_HashFunction) : (hash_function));
+	table->hash_compare = ((!hash_compare) ? (HashTable_Compare) : (hash_compare));
+	return table;
+}
+
+int
+HashTable_Insert(hashtable_t* table, const void* key,
+		size_t key_size, const void* data, size_t data_size)
+{
+	if (!table || !key || key_size == 0)
+	{
+		errno = EINVAL;
+		return -1;
+	}
+	int err;
+	const node_t* curr;
+	char* curr_key = NULL;
+	size_t hash = table->hash_function((void*) key) % table->buckets_no;
+	for (curr = LinkedList_GetFirst((table->buckets)[hash]); curr != NULL; curr = Node_GetNext(curr))
+	{
+		err = Node_CopyKey(curr, &curr_key);
+		if (err != 0) return -1;
+		if (table->hash_compare(key, curr_key) == 0) // duplicates are not allowed
+			return 0;
+		free(curr_key);
+	}
+	err = LinkedList_PushBack((table->buckets)[hash], key, key_size, data, data_size);
+	if (err != 0) return -1; // errno is now ENOMEM
+	return 1;
+}
+
+int
+HashTable_Find(const hashtable_t* table, const void* key)
+{
+	if (!table || !key)
+	{
+		errno = EINVAL;
+		return -1;
+	}
+	size_t hash = table->hash_function((void*) key) % table->buckets_no;
+	const node_t* curr;
+	char* curr_key;
+	int err;
+	for (curr = LinkedList_GetFirst((table->buckets)[hash]); curr != NULL; curr = Node_GetNext(curr))
+	{
+		err = Node_CopyKey(curr, &curr_key);
+		if (err != 0) return -1;
+		if (table->hash_compare(key, curr_key) == 0)
+			return 1;
+		free(curr_key);
+	}
+	return 0;
+}
+
+size_t
+HashTable_GetData(const hashtable_t* table, const void* key, void** data)
+{
+	if (!table || !key)
+	{
+		errno = EINVAL;
+		return 0;
+	}
+	size_t hash = table->hash_function((void*) key) % table->buckets_no;
+	const node_t* curr;
+	char* curr_key;
+	size_t size;
+	int err;
+	for (curr = LinkedList_GetFirst((table->buckets)[hash]); curr != NULL; curr = Node_GetNext(curr))
+	{
+		err = Node_CopyKey(curr, &curr_key);
+		if (err != 0) return -1;
+		if (table->hash_compare(key, curr_key) == 0)
+		{
+			size = Node_CopyData(curr, data);
+			free(curr_key);
+			return size;
+		}
+		free(curr_key);
+	}
+	*data = NULL;
+	return 0;
+}
+
+int
+HashTable_DeleteNode(hashtable_t* table, const void* key)
+{
+	if (!table || !key)
+	{
+		errno = EINVAL;
+		return -1;
+	}
+	size_t hash = table->hash_function((void*) key) % table->buckets_no;
+	const node_t* curr;
+	const node_t* prev;
+	char* curr_key;
+	int err;
+	for (curr = LinkedList_GetFirst((table->buckets)[hash]); curr != NULL; curr = Node_GetNext(curr))
+	{
+		err = Node_CopyKey(curr, &curr_key);
+		if (err != 0) return -1;
+		if (table->hash_compare(key, curr_key) == 0)
+		{
+			prev = Node_GetPrevious(curr);
+			if (prev) // Node can be easily deleted as it is not the first one
+				err = Node_Fold((node_t*) curr);
+			else // The linked list needs to be modified to properly address its first element
+				err = LinkedList_PopFront((table->buckets)[hash], NULL, NULL);
+			if (err != 0) return -1;
+			free(curr_key);
+			return 1;
+		}
+		else free(curr_key);
+	}
+	return 0;
+}
+
+void
+HashTable_Free(hashtable_t* table)
+{
+	if (table)
+	{
+		for (size_t i = 0; i < table->buckets_no; i++)
+		{
+			LinkedList_Free((table->buckets)[i]);
+		}
+		free(table->buckets);
+		free(table);
+	}
+	return;
+}
+
+void
+HashTable_Print(const hashtable_t* table)
+{
+	if (!table)
+	{
+		fprintf(stdout, "NULL\n");
+		return;
+	}
+	for (size_t i = 0; i < table->buckets_no; i++)
+	{
+		fprintf(stdout, "BUCKET NO. %lu:\n", i);
+		LinkedList_Print((table->buckets)[i]);
+	}
+	return;
+}
