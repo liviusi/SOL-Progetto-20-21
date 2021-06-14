f03e6aff46527ebfdb9a3100729ff8140adae726
Giacomo Trapani <giacomo.trapani01@libero.it>
2021-06-14 18:10:11 +0200

    Aggiunti define per server e client

---
+++ includes/server_defines.h
@@ -0,0 +1,32 @@
+/**
+ * @brief Defines used both by server and client.
+ * @author Giacomo Trapani.
+*/
+
+#ifndef _OPCODES_H_
+#define _OPCODES_H_
+
+#define SET_FLAG(mask, flag) mask |= flag
+#define RESET_MASK(mask) mask = 0
+
+#define O_CREATE 1
+#define O_LOCK 2
+#define IS_O_CREATE_SET(mask) mask & 1
+#define IS_O_LOCK_SET(mask) ((mask >> 1) & 1)
+
+#define OP_SUCCESS 0
+
+typedef enum opcodes
+{
+	OPEN,
+	CLOSE,
+	READ,
+	WRITE,
+	APPEND,
+	READ_N,
+	LOCK,
+	UNLOCK,
+	REMOVE
+} opcodes_t;
+
+#endif
