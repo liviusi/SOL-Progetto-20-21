3f1daefc9842a8d6387543f3afef64e84b0c37cb
Giacomo Trapani <giacomo.trapani01@libero.it>
2021-06-15 23:18:53 +0200

    Aggiunto un wrapper

---
+++ includes/wrappers.h
@@ -0,0 +1,15 @@
+#ifndef _WRAPPERS_H_
+#define _WRAPPERS_H_
+
+#include <server_defines.h>
+
+/**
+ * @brief Returns fatal error if called function output value is not equal to expected value.
+ * @param variable will be set to function_call output value
+ * @param expected_value value returned by function_call in case of success
+ * @param function_call actual function call
+*/
+#define RETURN_FATAL_IF_NEQ(variable, expected_value, function_call) \
+	if ((variable = function_call) != expected_value) return OP_FATAL;
+
+#endif
