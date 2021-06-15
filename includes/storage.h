09459cd91d92fb956c8fa515281ab8be5e9bc8c7
Giacomo Trapani <giacomo.trapani01@libero.it>
2021-06-15 23:19:34 +0200

    Iniziato ad abbozzare il server storage

---
+++ includes/storage.h
@@ -0,0 +1,26 @@
+/**
+ * @brief Header file for the filesystem.
+ * @author Giacomo Trapani.
+*/
+
+#ifndef _STORAGE__H_
+#define _STORAGE_H_
+
+#include <stdlib.h>
+typedef struct _storage storage_t;
+
+typedef enum _replacement_algo
+{
+	FIFO
+} replacement_algo_t;
+
+storage_t*
+Storage_Init(size_t, size_t, replacement_algo_t);
+
+void
+Storage_Free(storage_t*);
+
+int
+Storage_openFile(storage_t*, const char*, int, int);
+
+#endif
