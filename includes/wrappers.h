3fb77a87f063d9822bb039c886b40bdc6cfd0d42
Giacomo Trapani <giacomo.trapani01@libero.it>
2021-06-16 19:46:46 +0200

    Aggiunta una nuova macro

--- includes/wrappers.h
+++ includes/wrappers.h
@@ -12,4 +12,13 @@
 #define RETURN_FATAL_IF_NEQ(variable, expected_value, function_call) \
 	if ((variable = function_call) != expected_value) return OP_FATAL;
 
+/**
+ * @brief Exits with EXIT_FAILURE if called function output value is not equal to expected value.
+ * @param variable will be set to function_call output value
+ * @param expected_value value returned by function_call in case of success
+ * @param function_call actual function call
+*/
+#define EXIT_IF_NEQ(variable, expected_value, function_call) \
+	if ((variable = function_call) != expected_value) exit(EXIT_FAILURE);
+
 #endif
