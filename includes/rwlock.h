5d9f1986d19a511d693a4ea9f3098f02e341f4fd
Giacomo Trapani <giacomo.trapani01@libero.it>
2021-06-17 23:05:24 +0200

    Esposta la struct nel caso di debugging

--- includes/rwlock.h
+++ includes/rwlock.h
@@ -6,6 +6,16 @@
 #ifndef _RWLOCK_H_
 #define _RWLOCK_H_
 
+#ifdef DEBUG
+struct _rwlock
+{
+	pthread_mutex_t mutex; // mutex is used to control access to struct
+	pthread_cond_t cond;
+	unsigned int readers; // number of readers
+	bool pending_writer; // toggled on when there is a writer waiting
+};
+#endif
+
 // Struct fields are not exposed to maintain invariant.
 typedef struct _rwlock rwlock_t;
 
@@ -17,26 +27,26 @@ RWLock_Init();
 /**
 */
 int
-RWLock_ReadLock(rwlock_t*);
+RWLock_ReadLock(rwlock_t* lock);
 
 /**
 */
 int
-RWLock_ReadUnlock(rwlock_t*);
+RWLock_ReadUnlock(rwlock_t* lock);
 
 /**
 */
 int
-RWLock_WriteLock(rwlock_t*);
+RWLock_WriteLock(rwlock_t* lock);
 
 /**
 */
 int
-RWLock_WriteUnlock(rwlock_t*);
+RWLock_WriteUnlock(rwlock_t* lock);
 
 /**
 */
 void
-RWLock_Free(rwlock_t*);
+RWLock_Free(rwlock_t* lock);
 
 #endif
