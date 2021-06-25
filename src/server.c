b508427fefe1eb31c79aee914011a9af6af73faa
Giacomo Trapani <giacomo.trapani01@libero.it>
2021-06-25 20:18:30 +0200

    Aggiunta bozza del server

--- src/server.c
+++ src/server.c
@@ -13,20 +13,26 @@
 #include <unistd.h>
 
 #include <config.h>
+#include <server_defines.h>
 #include <storage.h>
+#include <utilities.h>
 #include <wrappers.h>
 #include <bounded_buffer.h>
 
 #define MAXPATH 108
 #define MAXCONNECTIONS 10
 #define PIPEBUFFERLEN 10
-#define TASKLEN 256
+#define TASKLEN 32
+#define REQUESTLEN 512
+#define MAXTASKS 4096
 
-volatile sig_atomic_t hard_sig_received = 0;
-volatile sig_atomic_t soft_sig_received = 0;
+#define TERMINATE_WORKER 0 // used to send a termination message
 
-void signal_handler(int);
+volatile sig_atomic_t terminate = 0;
+volatile sig_atomic_t no_more_clients = 0;
+
 static void* worker_routine(void*);
+void signal_handler(int);
 
 struct workers_args
 {
@@ -44,7 +50,7 @@ int main(int argc, char* argv[])
 	}
 	int err; // placeholder for functions' output values
 	int fd_socket; // socket's file descriptor
-	int fd_new_client;
+	int fd_new_client; // new client's fd
 	int pipe_worker2manager[2]; // pipe used for worker-manager communications
 	server_config_t* config = NULL; // server config
 	storage_t* storage = NULL; // server storage
@@ -55,12 +61,13 @@ int main(int argc, char* argv[])
 	unsigned long workers_pool_size = 0; // worker threads pool size
 	fd_set master_read_set; // read set
 	fd_set read_set; // copy of the original set
-	int fd_num = 0;
+	int fd_num = 0; // number of fds in set
 	bounded_buffer_t* tasks = NULL; // used to store tasks to be done
 	struct workers_args* workers_args = NULL; // worker threads' arguments
 	size_t online_clients = 0; // number of clients currently online
-	char pipe_buffer[PIPEBUFFERLEN];
-	int tmp;
+	char pipe_buffer[PIPEBUFFERLEN]; // buffer used by pipe
+	int pipe_msg; // message read from pipe
+	char new_task[TASKLEN]; // used to denote task to be added as a string
 
 	// --------------
 	// handle signals:
@@ -95,6 +102,9 @@ int main(int argc, char* argv[])
 	// initialize server storage
 	EXIT_IF_EQ(storage, NULL, Storage_Init((size_t) ServerConfig_GetMaxFilesNo(config),
 				(size_t) ServerConfig_GetStorageSize(config), FIFO), Storage_Init);
+	
+	// initialize tasks' bounded buffer
+	EXIT_IF_EQ(tasks, NULL, BoundedBuffer_Init(MAXTASKS), BoundedBuffer_Init);
 	
 	// initialize socket
 	EXIT_IF_EQ(err, 0, ServerConfig_GetSocketFilePath(config, &sockname),
@@ -131,10 +141,13 @@ int main(int argc, char* argv[])
 		EXIT_IF_EQ(err, -1, pthread_create(&(workers[i]), NULL,
 					&worker_routine, (void*) workers_args), pthread_create);
 
+	// -----------
+	// main loop
+	// -----------
 
 	while (1)
 	{
-		if (hard_sig_received) goto cleanup;
+		if (terminate) goto cleanup;
 
 		// fd set is to be reset as select does not
 		// preserve it
@@ -144,7 +157,7 @@ int main(int argc, char* argv[])
 		{
 			if (errno == EINTR)
 			{
-				if (soft_sig_received && online_clients == 0) break;
+				if (no_more_clients && online_clients == 0) break;
 				continue;
 			}
 			else
@@ -156,7 +169,7 @@ int main(int argc, char* argv[])
 
 		// loop through the fd set
 		// to find out which fd is now ready
-		for (size_t i = 0; i < fd_num + 1; i++)
+		for (int i = 0; i < fd_num + 1; i++)
 		{
 			if (FD_ISSET(i, &read_set)) // i is ready
 			{
@@ -164,18 +177,20 @@ int main(int argc, char* argv[])
 				// worker is done with a request
 				if (i == pipe_worker2manager[0])
 				{
-					EXIT_IF_EQ(err, -1, read(i, pipe_buffer, PIPEBUFFERLEN), read);
+					EXIT_IF_EQ(err, -1, readn((long) i, (void*) pipe_buffer,
+								PIPEBUFFERLEN), read);
 
-					tmp = atoi(pipe_buffer);
-					if (tmp) // tmp != 0 means no client left
+					EXIT_IF_NEQ(err, 1, sscanf(pipe_buffer, "%d", &pipe_msg), sscanf);
+
+					if (pipe_msg) // tmp != 0 means no client left
 					{
-						FD_SET(tmp, &master_read_set);
-						fd_num = MAX(tmp, fd_num);
+						FD_SET(pipe_msg, &master_read_set);
+						fd_num = MAX(pipe_msg, fd_num);
 					}
 					else // client left
 					{
 						online_clients--;
-						if (online_clients == 0 && soft_sig_received)
+						if (online_clients == 0 && no_more_clients)
 							goto cleanup;
 					}
 				}
@@ -183,7 +198,7 @@ int main(int argc, char* argv[])
 				{
 					EXIT_IF_EQ(fd_new_client, -1, accept(fd_socket, NULL, 0), accept);
 
-					if (soft_sig_received) close(fd_new_client);
+					if (no_more_clients) close(fd_new_client);
 					else
 					{
 						FD_SET(fd_new_client, &master_read_set);
@@ -193,9 +208,8 @@ int main(int argc, char* argv[])
 				}
 				else // new task from client
 				{
-					char new_task[TASKLEN];
 					memset(new_task, 0, TASKLEN);
-					snprintf(new_task, TASKLEN, "%lu", i);
+					snprintf(new_task, TASKLEN, "%d", i);
 					FD_CLR(i, &master_read_set);
 					if (i == fd_num) fd_num--;
 					// push ready file descriptor to task queue for workers
@@ -210,37 +224,212 @@ int main(int argc, char* argv[])
 
 
 	cleanup:
+		snprintf(new_task, TASKLEN, "%d", TERMINATE_WORKER);
+		for (size_t i = 0; i < (size_t) workers_pool_size; i++)
+			EXIT_IF_NEQ(err, 0, BoundedBuffer_Enqueue(tasks, new_task),
+						BoundedBuffer_Enqueue);
+		for (size_t i = 0; i < (size_t) workers_pool_size; i++)
+			pthread_join(workers[i], NULL);
 		ServerConfig_Free(config);
 		Storage_Free(storage);
+		BoundedBuffer_Free(tasks);
 		unlink(sockname);
 		free(sockname);
 		free(workers_args);
-		for (size_t i = 0; i < (size_t) workers_pool_size; i++)
-			pthread_join(workers[i], NULL);
 		free(workers);
 		close(pipe_worker2manager[0]);
 		close(pipe_worker2manager[1]);
 }
 
+/**
+ * @brief All worker threads work following the very
+ * same logic. At first, it is checked whether the descriptor
+ * to read from is a valid one (i.e. it is not equal to 0);
+ * then, it parses through the message read from the descriptor
+ * (at this point, it is assumed all messages are valid).
+ * @returns NULL.
+ * 
+*/
+static void* worker_routine(void* arg)
+{
+	struct workers_args* workers_args = (struct workers_args*) arg;
+	bounded_buffer_t* tasks = workers_args->tasks;
+	storage_t* storage = workers_args->storage;
+	int pipe_output_channel = workers_args->pipe_output_channel;
+	int err; // used as a placeholder for functions' output values
+	char* fd_ready_string; // fd to read from as a string
+	char* token = NULL; // token for strtok_r
+	char* saveptr = NULL; // saveptr for strtok_r
+	int fd_ready; // fd to read from
+	opcodes_t request_type; // type of request to be handled
+	char* request; // request as a string
+	char* tmp_request; // copy of request as a string
+	EXIT_IF_EQ(request, NULL, (char*) malloc(sizeof(char) * REQUESTLEN), malloc);
+	char pathname[MAXPATH]; // used to denote pathname for operations on storage
+	linked_list_t* evicted = NULL; // used to store evicted files
+	void* read_buf = NULL; // buffer used for reading operation
+	size_t read_size = 0; // size of read_buf
+	void* append_buf = NULL; // buffer used for append operation
+	size_t append_size = 0; // size of append_buf
+	int flags = 0; // used to denote flags for operations on storage
+	while(1)
+	{
+		// reset task string
+		fd_ready_string = NULL;
+		// read ready fd from tasks' queue
+		EXIT_IF_NEQ(err, 0, BoundedBuffer_Dequeue(tasks, &fd_ready_string),
+					BoundedBuffer_Dequeue);
+		EXIT_IF_NEQ(err, 1, sscanf(fd_ready_string, "%d", &fd_ready), sscanf);
+		if (fd_ready == TERMINATE_WORKER) // termination message
+		{
+			free(fd_ready_string);
+			break;
+		}
+		memset(request, 0, TASKLEN);
+		EXIT_IF_EQ(err, -1, readn((long) fd_ready, (void*) request,
+					REQUESTLEN), readn);
+		// request now contains the operation to be run
+		// and its arguments as a string
+		tmp_request = request;
+		token = strtok_r(tmp_request, " ", &saveptr);
+		EXIT_IF_NEQ(err, 1, sscanf(token, "%d", (int*) &request_type), sscanf);
+		switch (request_type)
+		{
+			case OPEN:
+				// get pathname
+				memset(pathname, 0, MAXPATH);
+				EXIT_IF_EQ(token, NULL, strtok_r(tmp_request, " ", &saveptr), token);
+				EXIT_IF_NEQ(err, 1, sscanf(token, "%s", pathname), sscanf);
+				// get flags
+				flags = 0;
+				EXIT_IF_EQ(token, NULL, strtok_r(tmp_request, " ", &saveptr), token);
+				EXIT_IF_NEQ(err, 1, sscanf(token, "%d", &flags), sscanf);
+				err = Storage_openFile(storage, pathname, flags, fd_ready);
+				// should somehow handle answer
+				break;
+
+			case CLOSE:
+				// get pathname
+				memset(pathname, 0, MAXPATH);
+				EXIT_IF_EQ(token, NULL, strtok_r(tmp_request, " ", &saveptr), token);
+				EXIT_IF_NEQ(err, 1, sscanf(token, "%s", pathname), sscanf);
+				err = Storage_closeFile(storage, pathname, fd_ready);
+				// should somehow handle answer
+				break;
+
+			case READ:
+				read_buf = NULL;
+				read_size = 0;
+				// get pathname
+				memset(pathname, 0, MAXPATH);
+				EXIT_IF_EQ(token, NULL, strtok_r(tmp_request, " ", &saveptr), token);
+				EXIT_IF_NEQ(err, 1, sscanf(token, "%s", pathname), sscanf);
+				// check whether file to be read's
+				// size and contents are to be saved
+				flags = 0;
+				EXIT_IF_EQ(token, NULL, strtok_r(tmp_request, " ", &saveptr), token);
+				EXIT_IF_NEQ(err, 1, sscanf(token, "%d", &flags), sscanf);
+				if (flags == 1)
+					err = Storage_readFile(storage, pathname, &read_buf, &read_size, fd_ready);
+				else 
+					err = Storage_readFile(storage, pathname, NULL, NULL, fd_ready);
+				// should somehow handle answer
+				break;
+
+			case WRITE:
+				evicted = NULL;
+				// get pathname
+				memset(pathname, 0, MAXPATH);
+				EXIT_IF_EQ(token, NULL, strtok_r(tmp_request, " ", &saveptr), token);
+				EXIT_IF_NEQ(err, 1, sscanf(token, "%s", pathname), sscanf);
+				// check whether dirname has been specified
+				token = strtok_r(tmp_request, " ", &saveptr);
+				if (!token)
+					err = Storage_writeFile(storage, pathname, NULL, fd_ready);
+				else
+					err = Storage_writeFile(storage, pathname, &evicted, fd_ready);
+				// should somehow handle answer
+				break;
+
+			case APPEND:
+				evicted = NULL;
+				append_buf = NULL;
+				append_size = 0;
+				// get pathname
+				memset(pathname, 0, MAXPATH);
+				EXIT_IF_EQ(token, NULL, strtok_r(tmp_request, " ", &saveptr), token);
+				EXIT_IF_NEQ(err, 1, sscanf(token, "%s", pathname), sscanf);
+				// get buffer to append
+				EXIT_IF_EQ(token, NULL, strtok_r(tmp_request, " ", &saveptr), token);
+				EXIT_IF_NEQ(err, 1, sscanf(token, "%s", (char*) append_buf), sscanf);
+				// get size of buffer
+				EXIT_IF_EQ(token, NULL, strtok_r(tmp_request, " ", &saveptr), token);
+				EXIT_IF_NEQ(err, 1, sscanf(token, "%lu", &append_size), sscanf);
+				// check whether dirname has been specified
+				token = strtok_r(tmp_request, " ", &saveptr);
+				if (!token)
+					err = Storage_appendToFile(storage, pathname, append_buf,
+								append_size, NULL, fd_ready);
+				else
+					err = Storage_appendToFile(storage, pathname, append_buf,
+								append_size, &evicted, fd_ready);
+				// should somehow handle answer
+				break;
+
+			case READ_N:
+				break;
+
+			case LOCK:
+				// get pathname
+				memset(pathname, 0, MAXPATH);
+				EXIT_IF_EQ(token, NULL, strtok_r(tmp_request, " ", &saveptr), token);
+				EXIT_IF_NEQ(err, 1, sscanf(token, "%s", pathname), sscanf);
+				err = Storage_lockFile(storage, pathname, fd_ready);
+				// should somehow handle answer
+				break;
+
+			case UNLOCK:
+				// get pathname
+				memset(pathname, 0, MAXPATH);
+				EXIT_IF_EQ(token, NULL, strtok_r(tmp_request, " ", &saveptr), token);
+				EXIT_IF_NEQ(err, 1, sscanf(token, "%s", pathname), sscanf);
+				err = Storage_unlockFile(storage, pathname, fd_ready);
+				// should somehow handle answer
+				break;
+
+			case REMOVE:
+				// get pathname
+				memset(pathname, 0, MAXPATH);
+				EXIT_IF_EQ(token, NULL, strtok_r(tmp_request, " ", &saveptr), token);
+				EXIT_IF_NEQ(err, 1, sscanf(token, "%s", pathname), sscanf);
+				err = Storage_removeFile(storage, pathname, fd_ready);
+				// should somehow handle answer
+				break;
+		}
+		
+		free(fd_ready_string);
+	}
+	free(request);
+	return NULL;
+}
+
+/**
+ * @brief Signal handler.
+*/
 void signal_handler(int signum)
 {
 	switch (signum)
 	{
 		case SIGINT:
 		case SIGQUIT:
-			hard_sig_received = 1;
+			terminate = 1;
 			break;
 
 		case SIGHUP:
-			soft_sig_received = 1;
+			no_more_clients = 1;
 			break;
 		
 		case SIGPIPE:
 			break;
 	}
-}
-
-static void* worker_routine(void* arg)
-{
-	return NULL;
 }
